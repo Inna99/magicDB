2fa063bc19daa9862134f2225bfb0219e824f3f8
inna <ikunygina@yandex.ru>
2021-02-05 23:05:33 +0300

    delete button

--- app.py
+++ app.py
@@ -17,26 +17,37 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.db = Connect()
         self.setupUi(self)
-        self.movies_list.setAcceptRichText(True) #  ?????????77
+        self.movies_list.setAcceptRichText(True)  # ?
         self.btn_update_movies.pressed.connect(self.printMovies)
         self.btn_create_movie.pressed.connect(self.addMovie)
+        self.btn_delete_by_id.pressed.connect(self.deleteById)
 
 
     def addMovie(self):
         line_title = self.line_title.text()
         line_production_year = self.line_production_year.text()
         viewed = self.checkBox_viewed.isChecked()
-        movie = Movies(title=line_title, production_year=date(int(line_production_year), 1, 1), viewed=viewed)
-        movie.save()
+        # movie = Movies(title=line_title, production_year=date(int(line_production_year), 1, 1), viewed=viewed)
+        # movie.save()
+        # ??? почему не так
+        Movies(title=line_title, production_year=date(int(line_production_year), 1, 1), viewed=viewed).save()
+        Movies.create(title=line_title, production_year=date(int(line_production_year), 1, 1), viewed=viewed)
+
         self.printMovies()
+        self.line_title.clear()
+        self.line_production_year.clear()
+        print(self.checkBox_viewed.setChecked(False))
 
     def printMovies(self):
-        print(list(Movies.select().dicts()))
         self.movies_list.clear()
         cursor = self.movies_list.textCursor()
         cursor.insertHtml('<ul>')
         for movie in Movies.select().dicts():
             # '<li>' +
-            film = f'{movie["title"]} {movie["production_year"].year} {"Просмотрено" if movie["viewed"] == True else "Непросмотрено"}'
-            self.movies_list.append(film)
+            self.movies_list.append(f'{movie["title"]} {movie["production_year"].year} {"Просмотрено" if movie["viewed"] == True else "Непросмотрено"}')
         cursor.insertHtml('</ul>')
+
+    def deleteById(self):
+        delete_id = Movies.select().order_by(Movies.id)[-1]
+        Movies.delete().where(Movies.id == delete_id).execute()
+        self.printMovies()
--- database/connection_orm.py
+++ database/connection_orm.py
@@ -1,17 +1,11 @@
 from peewee import *
-from datetime import date
-import os
-#  from config import BASE_DIR
 
-#  DB_PATH: str = os.path.abspath(os.path.join(BASE_DIR, 'db.db'))
 
-# db = SqliteDatabase('db.db')
 # Connect to a Postgres database.
 # driver , объект базы данных
-
 db = PostgresqlDatabase('postgres', user='postgres', password='mysecretpassword', host='localhost', port=5432)
 
-#     id = AutoField()  # Auto-incrementing primary key. по умолчанию(peewee)
+#  id = AutoField()  # Auto-incrementing primary key
 class BaseModel(Model):
     class Meta:
         database = db
@@ -44,28 +38,3 @@ class Connect(object):
             cls.db.connect()
             cls.db.create_tables([Producer, Movies])
         return cls.db
-
-
-
-
-
-# db.connect()
-# db.create_tables([Movies])
-#
-# TheLordOfTheRing = Movies(title='Властелин колец', production_year=date(2001, 1, 1), viewed=True)
-# Matrix = Movies(title='Матрица', production_year=date(1999, 1, 1))
-# TheLordOfTheRing.save()
-# Matrix.save()
-
-
-
-#print([movie for movie in Movies.select()])
-# print(type(Movies.select()))
-# query_set = Movies.select().where((Movies.title == 'Властелин колец') | (Movies.viewed == False))
-# for movie in query_set:
-#     print(movie.title, end=' ')
-#     movie.delete_instance()
-# print(f"{type(query_set)=}")
-# TheLordOfTheRing.delete_instance()
-# print(list(Movies.select().dicts()))
-# print(Movies.select().count())
